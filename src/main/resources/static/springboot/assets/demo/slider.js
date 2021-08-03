351deb5d8ed80f4abaadf86901cd1f3e7fe1a6a6
gag20 <gag2012@naver.com>
2021-08-03 19:49:36 +0900

    init

--- src/main/resources/static/springboot/assets/demo/slider.js
+++ src/main/resources/static/springboot/assets/demo/slider.js
@@ -1,7 +1,15 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
+var recov_slider = document.getElementById("recov_rate");
+var recov_output = document.getElementById("recov_demo");
+var death_slider = document.getElementById("death_rate");
+var death_output = document.getElementById("death_demo");
 
-slider.oninput = function() {
-  output.innerHTML = this.value;
+recov_output.innerHTML = recov_slider.value;
+death_output.innerHTML = death_slider.value;
+
+recov_slider.oninput = function() {
+  recov_output.innerHTML = this.value;
+}
+
+death_slider.oninput = function() {
+  death_output.innerHTML = this.value;
 }

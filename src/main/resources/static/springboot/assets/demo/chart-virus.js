f90c36d4fbb2a287feec9414ce5b7f7ed01fd65e
gag20 <gag2012@naver.com>
2021-08-12 17:09:40 +0900

    init

--- src/main/resources/static/springboot/assets/demo/chart-virus.js
+++ src/main/resources/static/springboot/assets/demo/chart-virus.js
@@ -235,10 +235,11 @@ function btn_cfg_click(){
   for(var i=1; i<total_day+1; i++){
     desfunction = Math.exp(-1*(ratio_alpha+ratio_mu)*(i/365));
     incfunction = Math.pow((ratio_beta*(i/365)), ratio_gamma);
-    iresult = incfunction * desfunction * population;
     wave_cfg.datasets[0].data.push(incfunction);
     wave_cfg.datasets[0].data1.push(desfunction);
     wave_cfg.datasets[0].data2.push(incfunction * desfunction);
+
+    iresult = incfunction * desfunction * population;
     wave_cfg.datasets[1].data.push(iresult);
   }
   console.log(wave_cfg.datasets[0].data);

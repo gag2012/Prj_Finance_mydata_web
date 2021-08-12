8d91c1b4592a1a92664060cfbcbca2976d179357
gag20 <gag2012@naver.com>
2021-08-12 10:12:19 +0900

    init

--- src/main/resources/static/springboot/assets/demo/chart-bar-demo.js
+++ src/main/resources/static/springboot/assets/demo/chart-bar-demo.js
@@ -7,12 +7,12 @@ var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["1월", "2월", "3월", "4월", "5월", "6월", "7월"],
     datasets: [{
       label: "Revenue",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [17471, 11467, 13415, 18919, 18339, 16623, 41380],
     }],
   },
   options: {
@@ -25,13 +25,13 @@ var myLineChart = new Chart(ctx, {
           display: false
         },
         ticks: {
-          maxTicksLimit: 6
+          maxTicksLimit: 7
         }
       }],
       yAxes: [{
         ticks: {
           min: 0,
-          max: 15000,
+          max: 50000,
           maxTicksLimit: 5
         },
         gridLines: {

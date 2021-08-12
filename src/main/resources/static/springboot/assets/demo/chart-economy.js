199c31db4a1b424210b1d1d5fd83beeed34e915a
gag20 <gag2012@naver.com>
2021-08-12 11:31:57 +0900

    init

--- src/main/resources/static/springboot/assets/demo/chart-economy.js
+++ src/main/resources/static/springboot/assets/demo/chart-economy.js
@@ -10,43 +10,36 @@ var myLineChart = new Chart(ctx, {
     labels: ["숙박 및 음식점업", "예술, 스포츠 및 여가관련 서비스업", "보건업 및 사회복지 서비스업", "보건업 및 사회복지 서비스업", "교육 서비스업", "전기, 가스, 증기 및 공기조절 공급업", "운수 및 창고업"
     ,"농업, 임업 및 어업"],
     datasets: [{
-      label: "Revenue",
+      label: "영업이익",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
       data: [-1193.645529, -612.6794598, -421.0335419, -133.3894592, 18339, -64.33639316, -61.21155584
       ,406.533829],
+      fill: false,
     }],
   },
   options: {
     title: {
       display: true,
       text: '영업이익 손해율(2018-2020)'
-         },
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false,
+    },
     scales: {
       xAxes: [{
-        time: {
-          unit: 'month'
-        },
         gridLines: {
-          display: false
         },
         ticks: {
-          maxTicksLimit: 7
         }
       }],
       yAxes: [{
         ticks: {
-          min: 0,
-          max: 50000,
-          maxTicksLimit: 5
         },
         gridLines: {
-          display: true
         }
       }],
     },
-    legend: {
-      display: false
-    }
   }
 });

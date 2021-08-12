96e610155baa24fffda4b0dd1bc70a3a4fbd9731
gag20 <gag2012@naver.com>
2021-08-12 10:02:04 +0900

    init

--- src/main/java/com/godcoder/myhome/controller/BoadController.java
+++ src/main/java/com/godcoder/myhome/controller/BoadController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/board")
 public class BoadController {
     @GetMapping("/list")
-    public String list(){
+    public String list() {
         return "board/list";
     }
 }

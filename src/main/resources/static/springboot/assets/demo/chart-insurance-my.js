c574caed4421d38c7804cf5f740389d7aa114e6f
gag20 <gag2012@naver.com>
2021-08-16 17:33:42 +0900

    init

--- src/main/resources/static/springboot/assets/demo/chart-insurance-my.js
+++ src/main/resources/static/springboot/assets/demo/chart-insurance-my.js
@@ -10,7 +10,7 @@ var myLineChart = new Chart(ctx, {
     labels: ["영업이익", "매출액", "임대료 등급", "상권변화 등급", "전염병 등급"],
     datasets: [{
         label: "나의 데이터",
-        data: [2, 5, 2, 4 ,5],
+        data: [2, 5, 1, 8 ,4],
         fill: true,
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
         borderColor: 'rgb(255, 99, 132)',
@@ -20,7 +20,7 @@ var myLineChart = new Chart(ctx, {
         pointHoverBorderColor: 'rgb(255, 99, 132)'
       }, {
         label: "평균 데이터",
-        data: [1, 4, 5, 3, 1],
+        data: [3, 4, 5, 3, 4],
         fill: true,
         backgroundColor: 'rgba(54, 162, 235, 0.2)',
         borderColor: 'rgb(54, 162, 235)',

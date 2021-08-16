b39f7355f8b9c2479e7c6d1d41e16b080143a428
gag20 <gag2012@naver.com>
2021-08-17 01:34:40 +0900

    init

--- src/main/resources/static/springboot/assets/demo/insurance_analysis.js
+++ src/main/resources/static/springboot/assets/demo/insurance_analysis.js
@@ -1,2 +1,4 @@
-document.getElementById("textTest").innerHTML = document.getElementById("job_select").value;
-document.getElementById("textTest2").innerHTML = 123;
+function btn_isr_click(){
+    document.getElementById("textTest").innerHTML = document.getElementById("job_select").value;
+    document.getElementById("textTest2").innerHTML = 123;
+}

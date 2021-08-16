67b0e0ce91795f22559d66e03783faeb264d5efb
gag20 <gag2012@naver.com>
2021-08-17 01:44:17 +0900

    init

--- src/main/resources/static/springboot/assets/demo/insurance_analysis.js
+++ src/main/resources/static/springboot/assets/demo/insurance_analysis.js
@@ -1,4 +1,7 @@
 function btn_isr_click(){
     document.getElementById("textTest").innerHTML = document.getElementById("job_select").value;
-    document.getElementById("textTest2").innerHTML = 123;
+    document.getElementById("textTest2").innerHTML = document.getElementById("location_select").value;
+    document.getElementById("textTest3").innerHTML = document.getElementById("duration_select").value;
+    document.getElementById("textTest4").innerHTML = document.getElementById("income_select").value;
+    document.getElementById("textTest5").innerHTML = document.getElementById("virus_select").value;
 }

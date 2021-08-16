776908d68aef613bcd0dcd212640fd5420ea063e
gag20 <gag2012@naver.com>
2021-08-16 16:03:25 +0900

    init

---
+++ src/main/resources/static/springboot/assets/demo/chart-insurance-my.js
@@ -0,0 +1,57 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+// Bar Chart Example
+var ctx = document.getElementById("insuranceChart");
+var myLineChart = new Chart(ctx, {
+  type: 'radar',
+  data: {
+    labels: ["영업이익", "피해 매출액", "임대료"],
+    datasets: [{
+        label: 'My First Dataset',
+        data: [65, 59, 90],
+        fill: true,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgb(255, 99, 132)',
+        pointBackgroundColor: 'rgb(255, 99, 132)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgb(255, 99, 132)'
+      }, {
+        label: 'My Second Dataset',
+        data: [28, 48, 40],
+        fill: true,
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgb(54, 162, 235)',
+        pointBackgroundColor: 'rgb(54, 162, 235)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgb(54, 162, 235)'
+      }],
+  },
+  options: {
+    title: {
+      display: true,
+      text: '영업이익 손해 변화율(2018 ~ 2020)'
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false,
+    },
+    scales: {
+      xAxes: [{
+        gridLines: {
+        },
+        ticks: {
+        }
+      }],
+      yAxes: [{
+        ticks: {
+        },
+        gridLines: {
+        }
+      }],
+    },
+  }
+});

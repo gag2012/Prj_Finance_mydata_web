141c94a98840961d608e73055f5b4e746849ab09
gag20 <gag2012@naver.com>
2021-08-17 00:58:46 +0900

    init

--- src/main/resources/static/springboot/assets/demo/chart-insurance-my.js
+++ src/main/resources/static/springboot/assets/demo/chart-insurance-my.js
@@ -2,53 +2,52 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
-// Bar Chart Example
-var ctx = document.getElementById("analysisChart");
-var myLineChart = new Chart(ctx, {
-  type: 'radar',
-  data: data = {
-          labels: [
-            '매출액',
-            '영업이익',
-            '상권변화 등급',
-            '임대료 등급',
-            '전염병 등급',
-          ],
-          datasets: [{
-            label: '나의 데이터',
-            data: [3, 1, 5, 4, 3],
-            fill: true,
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgb(255, 99, 132)',
-            pointBackgroundColor: 'rgb(255, 99, 132)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgb(255, 99, 132)'
-          }, {
-            label: '평균 데이터',
-            data: [4, 5, 4, 4, 3],
-            fill: true,
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            borderColor: 'rgb(54, 162, 235)',
-            pointBackgroundColor: 'rgb(54, 162, 235)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgb(54, 162, 235)'
-          }]
-  },
-  options: {
-      elements: {
-        line: {
-          borderWidth: 3
+var options = {
+    responsive: false,
+    maintainAspectRatio: true,
+    scale: {
+        ticks: {
+            beginAtZero: true,
+            max: 8
         }
-      },
-      scales: {
-            ticks: {
-                beginAtZero: true,
-                max: 8,
-                min: 0,
-                stepSize: 1
-            }
-      }
-  }
+    }
+};
+
+var dataLiteracy = {
+    labels: [
+        "영업이익", "매출액", "상권변화 등급", "임대료 등급", "전염병 등급"
+    ],
+    datasets: [{
+        label: "나의 데이터",
+         backgroundColor: 'rgba(255, 99, 132, 0.2)',
+         borderColor: 'rgb(255, 99, 132)',
+         pointBackgroundColor: 'rgb(255, 99, 132)',
+         pointBorderColor: '#fff',
+         pointHoverBackgroundColor: '#fff',
+         pointHoverBorderColor: 'rgb(255, 99, 132)',
+        data: [
+            2, 3, 4, 1, 2
+        ]
+    },
+    {
+        label: "평균 데이터",
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgb(54, 162, 235)',
+        pointBackgroundColor: 'rgb(54, 162, 235)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgb(54, 162, 235)',
+        data: [
+            3, 5, 4, 7, 3
+        ]
+    }]
+};
+
+var ctx = document.getElementById("analysisChart");
+var myRadarChart = new Chart(ctx, {
+    type: 'radar',
+    data: dataLiteracy,
+    options: options
 });
+
+console.log(myRadarChart);
